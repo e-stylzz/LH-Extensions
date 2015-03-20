c56f918e143f9975cf867a603a67e806a517a3f6
Eric Barb <eric.barb@gmail.com>
2015-03-20 15:03:44 -0400

    Initial Load

---
+++ R/LH-Extensions.R
@@ -0,0 +1,21 @@
+#Author:  Eric Barb
+#Description:  Working with Extensions and their probability
+
+
+#Get basics in order
+setwd("~/GitHub/LH-Extensions")
+extensions <- read.csv("~/GitHub/Datasets/LH-Extensions/extmain.csv")
+
+
+#Bring in Libraries
+library(rpart)
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+library(randomForest)
+library(party)
+
+#Create Train and Test Sets
+extensions$group <- runif(dim(extensions)[1])
+test <- subset(extensions, extensions$group <= 0.1)
+train <- subset(extensions, extensions$group > 0.1)
